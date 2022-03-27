7ec5ff8925b0643db00dd8d43aedc31017fe116d
Akira-Matsu <resp.uw.t.1221@gmail.com>
2022-03-28 03:33:09 +0900

    Add subgraph api

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-  export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-  export const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+  export const contractAddress = "0x36Af6EC39f2d22714C497234BFe3B1E65523b251"
+  export const ownerAddress = "0x356977C9d5155eBfFcCC87B1612032561559722A"
   
